1a61ebadff8f90f9a96c6f9da8cb052711eee13d
fanpei <fanpei@mycompany.com>
2018-10-25 14:52:30 +0000

    26字节gps数据异或运算bug修正

--- src/main/java/soft/common/BitCheckUtil.java
+++ src/main/java/soft/common/BitCheckUtil.java
@@ -11,6 +11,25 @@ public class BitCheckUtil extends StaticClass {
 	/**
 	 * 异或运算
 	 * 
+	 * @param bytes 待计算数组
+	 * @param begin 开始位置
+	 * @param len   长度
+	 * @return
+	 */
+	public static byte xorCheck(byte[] bytes, int begin, int len) {
+		if (len > bytes.length)
+			throw new ArrayIndexOutOfBoundsException("end is more than bytes.len");
+
+		byte xorResult = 0;
+		for (int i = begin; i < len; i++) {
+			xorResult ^= bytes[i];
+		}
+		return xorResult;
+	}
+
+	/**
+	 * 异或运算
+	 * 
 	 * @param srcResult
 	 * @param bytes
 	 * @return
