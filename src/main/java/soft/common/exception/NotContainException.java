6c629a7092449eb33e3fd65cbf0edac661d90672
fanpei <fanpei@mycompany.com>
2018-10-30 16:01:15 +0000

    新增NotContainException

---
+++ src/main/java/soft/common/exception/NotContainException.java
@@ -0,0 +1,14 @@
+package soft.common.exception;
+
+public class NotContainException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NotContainException(String dsrc) {
+		super(dsrc);
+	}
+
+}
