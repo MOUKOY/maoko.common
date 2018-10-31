d93f189df6320b96c8659901192c59779f3c8dd0
fanpei <fanpei@mycompany.com>
2018-10-31 15:39:32 +0000

    lock协议返回组装完成

--- src/main/java/soft/common/exception/OutOfRangeException.java
+++ src/main/java/soft/common/exception/OutOfRangeException.java
@@ -1,20 +1,20 @@
 package soft.common.exception;
 
 /**
- * 不包含异常
+ * 超出范围异常
  * 
  * @author fanpei
- * @date 2018-10-31 17:18
+ * @date 2018-10-31 17:17
  *
  */
-public class NotContainException extends Exception {
+public class OutOfRangeException extends Exception {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public NotContainException(String dsrc) {
+	public OutOfRangeException(String dsrc) {
 		super(dsrc);
 	}
 
