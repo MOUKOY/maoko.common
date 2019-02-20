379dd2126a5cf10cf9729819a68ae32920eab2b1
fanpei <fanpei@mycompany.com>
2019-02-20 10:10:43 +0000

    1.新增log4j-1.2-api 依赖 2.修复在springmvc先初始化日志的情况下不能打印日志的问题

--- src/main/java/soft/common/log/Log4j2Writer.java
+++ src/main/java/soft/common/log/Log4j2Writer.java
@@ -1,12 +1,22 @@
 package soft.common.log;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLDecoder;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.ConfigurationFactory;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.DefaultConfiguration;
+import org.apache.logging.log4j.core.impl.Log4jContextFactory;
+import org.apache.logging.log4j.spi.LoggerContextFactory;
 
 import moukoy.sdkcommon.SDKCommon;
 import soft.common.file.PathUtil;
@@ -25,33 +35,56 @@ public class Log4j2Writer implements IWriteLog {
 	 * @throws IOException
 	 */
 	public static void init() throws LoginitException {
+		FileInputStream inConfStream = null;
+		LoggerContext context = null;
 		try {
 			String runPath = System.getProperty(SDKCommon.RUNPATH);
 			String filepath = PathUtil.combinePath(runPath, CONFIGFILE);
 			File file = new File(filepath);
 			String configPath = file.getAbsolutePath();
-			System.out.println("log4j2.xml path " + configPath);
+			System.out.println("setting log4j2.xml path " + configPath);
 			if (!file.exists())// 不存在使用默认值
 			{
-				InputStream in = Log4j2Writer.class.getClassLoader().getResourceAsStream(CONFIGFILE);
-				FileUtils.copyInputStreamToFile(in, file);
-				System.err.println(CONFIGFILE + " is not found,sys will use default config");
+				InputStream in = new FileResource(CONFIGFILE).getResourceStream();
+				if (in != null) {
+					FileUtils.copyInputStreamToFile(in, file);
+					System.err.println(CONFIGFILE + " is not found,sys will use default config");
+				}
 			}
-			System.setProperty("log4j.configurationFile", configPath);
-			// 异步模式
-			// System.setProperty("Log4jContextSelector",
-			// "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
-
-			// ConfigurationSource source = new ConfigurationSource(new
-			// FileInputStream(file));
-			// LoggerContext context = Configurator.initialize(null, source);
-			// XmlConfiguration xmconfig = new XmlConfiguration(context, source);
-			// context.start(xmconfig);
+			/*
+			 * System.setProperty("log4j.configurationFile", url.toString()); LoggerContext
+			 * context = (LoggerContext) LogManager.getContext(false);
+			 * context.reconfigure(); 这种方法也行，但是代码中修改系统属性，不太好维护
+			 */
+			// context = (LoggerContext) LogManager.getContext(false);
+			// context.reconfigure();
+			inConfStream = new FileInputStream(file);
+			ConfigurationSource source = new ConfigurationSource(inConfStream, file.toURI().toURL());
+			context = Configurator.initialize(null, source);
+			
+			//未初始化成功重新加载-与sprigmvc集成时，日志先启动问题修复
+			if (context.getConfiguration() instanceof DefaultConfiguration) {
+				inConfStream = new FileInputStream(file);
+				source = new ConfigurationSource(inConfStream, file.toURI().toURL());
+				final Configuration config = ConfigurationFactory.getInstance().getConfiguration(context, source);
+				context.start(config);
+			}
+			Log4j2Writer log = new Log4j2Writer(Log4j2Writer.class);
+			log.info("log4j has been inited sucessful");
 
 		} catch (Exception e) {
 			throw new LoginitException("日志初始化失败", e);
-		}
+		} finally {
 
+			try {
+				if (inConfStream != null) {
+					inConfStream.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	/**
