f98e9a3f9d8fc2184dfff1eec6be3b9571dd9ca0
fanpei <fanpei1413@163.com>
2019-06-24 16:59:16 +0800

    新增key-vlue对象

---
+++ src/main/java/maoko/common/model/AKeyValue.java
@@ -0,0 +1,30 @@
+package maoko.common.model;
+
+public abstract class AKeyValue {
+
+	protected String key;
+	protected String value;
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public AKeyValue(String key, String value) {
+		this.key = key;
+		this.value = value;
+
+	}
+
+}
