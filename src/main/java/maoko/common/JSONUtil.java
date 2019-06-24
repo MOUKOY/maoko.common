48c50b20f2c960c11040fb95fbfd473940dd82b6
fanpei <fanpei1413@163.com>
2019-06-24 17:12:16 +0800

    新增数组转json

--- src/main/java/maoko/common/JSONUtil.java
+++ src/main/java/maoko/common/JSONUtil.java
@@ -1,5 +1,6 @@
 package maoko.common;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,6 +9,8 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+
+import maoko.common.model.AKeyValue;
 
 /**
  * JSON助手
@@ -83,6 +86,53 @@ public class JSONUtil extends StaticClass {
 	 */
 	public static String genJsonStr(Object obj) {
 		return gson.toJson(obj);
+	}
+
+	/**
+	 * 二维数组转换Json
+	 * 
+	 * @param objs
+	 * @return
+	 */
+	public static String genArrayJsonStr(List<AKeyValue[]> objs) {
+		if (objs != null) {
+			JsonArray[] jas = new JsonArray[objs.size()];
+			Iterator<AKeyValue[]> it = objs.iterator();
+			int index = 0;
+			while (it.hasNext()) {
+				JsonArray ja = new JsonArray();
+				jas[index] = ja;
+				AKeyValue[] e = it.next();
+				for (AKeyValue aKeyValue : e) {
+					ja.add(JSONUtil.genJsonStr(aKeyValue));
+				}
+				index++;
+			}
+			return gson.toJson(jas);
+		} else
+			return null;
+	}
+
+	public static <T> List<List<T>> genArrayObjects(String jsonsStr, Class<T> classType) {
+		List<List<T>> ts = null;
+		if (jsonsStr != null) {
+			ts = new LinkedList<>();
+			JsonArray[] arrays = gson.fromJson(jsonsStr, JsonArray[].class);
+			if (arrays != null) {
+				for (JsonArray jsa : arrays) {
+					int len = jsa.size();
+					List<T> values = new ArrayList<>(len);
+					for (int i = 0; i < len; i++) {
+						// String jsonStr=jsa.get(i).getAsString();
+						String jsonStr = gson.toJson(jsa.get(i));
+						T t = gson.fromJson(jsonStr, classType);
+						values.add(t);
+					}
+					ts.add(values);
+				}
+			}
+		}
+		return ts;
 	}
 
 }
